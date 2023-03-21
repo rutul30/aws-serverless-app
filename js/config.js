39de3d765c550c100049be749dba0bb85806b993
rutul30 <rutuljoshi30@gmail.com>
2023-03-21 16:53:04 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_IRQ2xsoeV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6nkvfjj3tlvr6euv57q52949pc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
