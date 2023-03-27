268a77a957ab2dba4cdf1b3de5da7172186384b5
rutul30 <94869513+rutul30@users.noreply.github.com>
2023-03-27 15:00:29 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: "https://main.dezey6vhy1vmy.amplifyapp.com/" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: "https://keybh609di.execute-api.us-east-1.amazonaws.com/prod" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
